98bac5b113e269d948fac109875305a915802a58
GoncharovUV <goncharov.uv@gmail.com>
2022-02-16 15:26:43 +0300

    send mail confirmation code

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,17 +1,5 @@
-import random
-import string
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
-CONFIRMATION_CODE_LEN = 20
-
-
-def get_confirmation_code():
-    return ''.join(random.choices(
-        string.digits + string.ascii_uppercase,
-        k=CONFIRMATION_CODE_LEN
-    ))
 
 
 class User(AbstractUser):
