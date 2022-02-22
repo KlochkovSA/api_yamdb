730c99c21b6d6540b0b4afa6701b712ede06425f
GoncharovUV <goncharov.uv@gmail.com>
2022-02-22 14:27:58 +0300

    test_01 2 failed

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -1,13 +1,40 @@
 from rest_framework import permissions
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
 
 
-class AdminPermission(permissions.BasePermission):
-    # def has_permission(self, request, view):
-    #     return (request.user.is_authenticated
-    #             or request.method in permissions.SAFE_METHODS)
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_admin
 
     def has_object_permission(self, request, view, obj):
-        return request.user.role == User.ADMIN
+        return request.user.is_authenticated and request.user.is_admin
+
+class IsAdminOrOnlyRead(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and request.user.is_admin
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and request.user.is_admin
+
+
+class IsModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_moderator
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and request.user.is_moderator
+
+
+class IsSuperuser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_superuser
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and request.user.is_superuser
+
+class IsAuthor(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and request.user == obj.author
