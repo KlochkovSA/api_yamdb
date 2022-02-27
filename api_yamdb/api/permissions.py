a6581ff30b793d7782415da23e09b97c6a4a5666
KlochkovSA <sergeyalex1811@yandex.ru>
2022-02-27 21:57:12 +0300

    refactored permissions.py

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,32 +1,17 @@
 from rest_framework import permissions
 
 
-class OwnerOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
-
-    def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
-
-
-class ReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
-
-
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'admin'
+        return request.user.is_authenticated and request.user.is_admin
 
 
-class ModeratorPermission(permissions.BasePermission):
-
+class IsModerator(permissions.BasePermission):
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
+        return (request.user.is_authenticated
+                and request.user.is_moderator)
 
+
+class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user.role == 'mr'
+        return obj.author == request.user
