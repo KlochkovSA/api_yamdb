bbdc7d732226404c35ee06e012834eee0473e8e9
KlochkovSA <sergeyalex1811@yandex.ru>
2022-02-27 21:55:35 +0300

    Models refactored

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -110,6 +110,12 @@ class Review(models.Model):
         verbose_name = 'Отзыв'
         verbose_name_plural = 'Отзывы'
         ordering = ('-pub_date',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='Unique review'
+            )
+        ]
 
     def __str__(self):
         return self.text
