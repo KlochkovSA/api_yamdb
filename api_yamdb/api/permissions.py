0e9f7a090166ead6f9ffdf26d2d1e80eaebd9029
KlochkovSA <sergeyalex1811@yandex.ru>
2022-03-03 17:14:37 +0300

    permissions codestyle fix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -9,8 +9,8 @@ class IsAdmin(permissions.BasePermission):
 class OwnerAndStaffPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         has_auth = request.user.is_authenticated
-        is_owner = has_auth and obj.author == request.user
-        is_admin = has_auth and request.user.is_admin
-        is_moderator = has_auth and request.user.is_moderator
+        is_owner = obj.author == request.user
+        is_admin = request.user.is_admin
+        is_moderator = request.user.is_moderator
 
-        return is_owner or is_moderator or is_admin
+        return has_auth and (is_owner or is_moderator or is_admin)
