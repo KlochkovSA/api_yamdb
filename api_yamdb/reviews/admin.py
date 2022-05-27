0cb8ea429529b3814978a910cc523eaf9743594c
KlochkovSA <sergeyalex1811@yandex.ru>
2022-05-27 13:56:12 +0400

    Registered models in admin site

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
-from .models import Comment, Review
+from .models import Category, Comment, Genre, Review, Title
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Review)
